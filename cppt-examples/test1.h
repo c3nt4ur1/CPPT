a4d277ad48980f11e6a89176ca37e9e1ba901433
ANBonin <adriano.bonin@outlook.com>
2025-10-30 11:06:22 -0300

    Added some test cases to example test1

--- cppt-examples/test1.h
+++ cppt-examples/test1.h
@@ -20,11 +20,25 @@ bool test3(){
     return IntAssert("INT ASSERT FAILURE", 5, 3).passed;
 }
 
+bool test4(){
+    return DoubleAssert("DoubleAssert Test", 2.00, 2.00).passed;
+}
+
+bool test5(){
+    return DoubleAssert("DoubleAssert Failure", 2.00, 3.00).passed;
+}
+
+bool test6(){
+    return DoubleAssert("DoubleAssert with integer", 2, 2.00).passed;
+}
 
 void registerCases(){ //Pushes the test cases to the function<bool()> queue
     Controller::addTestCase(test1);
     Controller::addTestCase(test2);
     Controller::addTestCase(test3);
+    Controller::addTestCase(test4);
+    Controller::addTestCase(test5);
+    Controller::addTestCase(test6);
 }
 
 
