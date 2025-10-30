b7aafde42c115a971fa5fcdeb2138a51ebf1625f
ANBonin <adriano.bonin@outlook.com>
2025-10-30 11:10:19 -0300

    Standardized floating point tolerance; Added DoubleAssertDiffers class

--- cppt-lib/DoubleAssert.h
+++ cppt-lib/DoubleAssert.h
@@ -36,7 +36,81 @@ public:
     }
 
     DoubleAssert(int testNum, string testName, double expected, double actual)
-            : Assertion(std::fabs(expected - actual) < 1e-9, testName, testNum){
+            : Assertion(std::fabs(expected - actual) < 1e-10, testName, testNum){
+        this->expected = expected;
+        this->actual = actual;
+
+        this->passed = this->assertedCondition;
+
+        if(!this->run()){
+            cerr << "Failed to push out assertion\n";
+            exit(102);
+        }
+
+        if(!this->assertedCondition){
+            printGottenOutput();
+        }
+    }
+
+private:
+
+    inline bool run(){
+        return outputAssertion();
+    }
+
+    void printGottenOutput() const{
+        Test::resultsOutput << "\tEXPECTED: " << this->expected << " ACTUAL: " << this->actual << endl;
+    }
+
+    bool outputAssertion() override{
+        if(!Test::resultsOutput){
+            cerr << "Unloaded target file\n";
+            return false;
+        }
+
+        Test::resultsOutput << this->testName;
+        if(this->testNumber != -1){
+            Test::resultsOutput << " " << this->testNumber;
+        }
+
+        if(this->passed){
+            Test::resultsOutput << " " << "PASSED" << endl;
+        }else{
+            Test::resultsOutput << " " << "FAILED" << endl;
+        }
+
+        return true;
+    }
+
+    double expected;
+    double actual;
+
+};
+
+class DoubleAssertDiffers : public Assertion{
+
+public:
+    bool passed;
+
+    DoubleAssertDiffers(string testName, double expected, double actual)
+            : Assertion(std::fabs(expected - actual) > 1e-10, testName){
+
+        this->expected = expected;
+        this->actual = actual;
+
+        this->passed = this->assertedCondition;
+
+        if(!this->run()){
+            cerr << "Failed to push out assertion\n";
+            exit(102);
+        }
+        if(!this->assertedCondition){
+            printGottenOutput();
+        }
+    }
+
+    DoubleAssertDiffers(int testNum, string testName, double expected, double actual)
+            : Assertion(std::fabs(expected - actual) > 1e-10, testName, testNum){
         this->expected = expected;
         this->actual = actual;
 
